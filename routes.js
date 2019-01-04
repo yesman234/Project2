5ddc66e3ab04a33fef1395b16e1227cae01799a8
yesman234 <mdjonesonly310@gmail.com>
2019-01-03 18:37:35 -0800

    push

--- routes.js
+++ routes.js
@@ -4,10 +4,12 @@ module.exports = function(app){
 		const users = require('./routes/users');
 		const trips = require('./routes/trips');
 		const pricing = require('./routes/pricing');
+		const drone = require('./routes/drone');
 
 		app.use('/', application);
 		app.use('/users', users);
 		app.use('/trips', trips);
 		app.use('/pricing', pricing);
+		app.use('/drone', drone);
     //other routes..
 }
